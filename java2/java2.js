1ba8ed3a5576be0e36d728072f1780a1bb2d2ed7
Takács Máté 2 <matetakacs0505@gmail.com>
2026-04-07 18:40:59 +0200

    adatok módosítása

--- java2/java2.js
+++ java2/java2.js
@@ -37,6 +37,38 @@ const hozzaadas = () => {
     tmb.push(`${datum}\t${nev}\t${helyszin}`);
     buildTable();
 }
+
+const modositas = (idx) => {
+    const data = tmb[idx].split('\t');
+    const datum = document.getElementById('datum');
+    const nev = document.getElementById('nev');
+    const helyszin = document.getElementById('helyszin');
+
+    const gombok = document.getElementById('gombok');
+    gombok.innerHTML = `<button type="button" onclick="modositasMegerosites(${idx})">Megerősítés</button><button type="button" onclick="modositasMegse()">Mégse</button>`;
+
+    datum.value = data[0].replaceAll('.', '-');
+    nev.value = data[1];
+    helyszin.value = data[2];
+}
+
+const modositasMegerosites = (idx) => {
+    const datum = document.getElementById('datum');
+    const nev = document.getElementById('nev');
+    const helyszin = document.getElementById('helyszin');
+
+    tmb[idx] = `${datum.value.replaceAll('-', '.')}\t${nev.value}\t${helyszin.value}`;
+    buildTable();
+}
+
+const modositasMegse = () => {
+    const inputs = document.getElementsByTagName('input');
+    for (const input of inputs) input.value = '';
+
+    const gombok = document.getElementById('gombok');
+    gombok.innerHTML = '<button onclick="hozzaadas()">Új rekord</button>';
+}
+
 const torles = (idx) => {
     tmb.splice(idx, 1);
     buildTable();
