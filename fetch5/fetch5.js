81253afb122d9543ef80c55182ef0a2c8d984001
Takács Máté 2 <matetakacs0505@gmail.com>
2026-04-12 18:53:33 +0200

    fetchapi elkezdve

---
+++ fetch5/fetch5.js
@@ -0,0 +1,76 @@
+const adatok = document.getElementById('adatok');
+const gombok = document.getElementById('gombok');
+let tmb = [];
+
+
+const fetchData = async () => {
+    const response = await fetch('/backend/api.php');
+    tmb = (await response.json()).readData;
+
+    buildTable();
+}
+
+const buildTable = () => {
+    adatok.innerHTML = '';
+
+    tmb.map((data, idx) => {
+        const tr = document.createElement('tr');
+        const items = Object.values(data);
+        for (const item of items) {
+            const td = document.createElement('td');
+            td.innerText = item;
+            tr.appendChild(td);
+        }
+        const td = document.createElement('td');
+        td.innerHTML = `<button onclick="modositas(${idx})">Módosítás</button><button onclick="torles(${idx})">Törlés</button>`
+        tr.appendChild(td);
+        adatok.appendChild(tr);
+    })
+}
+
+const hozzaadas = () => {
+    const datum = document.getElementById('datum').value;
+    const nev = document.getElementById('nev').value;
+    const helyszin = document.getElementById('helyszin').value;
+
+    tmb.push(`${datum}\t${nev}\t${helyszin}`);
+    buildTable();
+}
+
+const modositas = (idx) => {
+    const data = tmb[idx].split('\t');
+    const datum = document.getElementById('datum');
+    const nev = document.getElementById('nev');
+    const helyszin = document.getElementById('helyszin');
+
+    const gombok = document.getElementById('gombok');
+    gombok.innerHTML = `<button type="button" onclick="modositasMegerosites(${idx})">Megerősítés</button><button type="button" onclick="modositasMegse()">Mégse</button>`;
+
+    datum.value = data[0].replaceAll('.', '-');
+    nev.value = data[1];
+    helyszin.value = data[2];
+}
+
+const modositasMegerosites = (idx) => {
+    const datum = document.getElementById('datum');
+    const nev = document.getElementById('nev');
+    const helyszin = document.getElementById('helyszin');
+
+    tmb[idx] = `${datum.value.replaceAll('-', '.')}\t${nev.value}\t${helyszin.value}`;
+    buildTable();
+}
+
+const modositasMegse = () => {
+    const inputs = document.getElementsByTagName('input');
+    for (const input of inputs) input.value = '';
+
+    const gombok = document.getElementById('gombok');
+    gombok.innerHTML = '<button onclick="hozzaadas()">Új rekord</button>';
+}
+
+const torles = (idx) => {
+    tmb.splice(idx, 1);
+    buildTable();
+}
+
+fetchData();
