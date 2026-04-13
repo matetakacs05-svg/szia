668ed729e33687f45f939aac964a8d9e6c37bc63
Takács Máté 2 <matetakacs0505@gmail.com>
2026-04-13 18:06:29 +0200

    axios form és adat megjelenítés

--- axios6/src/App.tsx
+++ axios6/src/App.tsx
@@ -24,114 +24,53 @@ function App() {
         fetchData();
     }, []);
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    return (
+      <div>
+          <form className={'container'}>
+            <div>
+                <label htmlFor={'nev'}>Név</label><br/>
+                <input type={'text'} id={'nev'} name={'nev'} required />
+            </div>
+            <div>
+                <label htmlFor={'nem'}>Nem</label><br/>
+                <select id={'nem'} name={'nem'}>
+                    <option value={''} disabled selected>Válassz...</option>
+                    <option value={'F'}>Férfi</option>
+                    <option value={'N'}>Nő</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor={'szuldat'}>Szül. dátum</label>
+                <input type={'date'} id={'szuldat'} name={'szuldat'} required />
+            </div>
+            <div>
+                <label htmlFor={'nemzet'}>Nemzet</label>
+                <input type={'text'} id={'nemzet'} name={'nemzet'} required />
+            </div>
+            { editIdx === null ?
+                <button type={'button'}>Hozzáadás</button>
+                :
+                <>
+                    <button type={'button'}>Módosítás</button>
+                    <button type={'button'}>Mégse</button>
+                </>
+            }
+          </form>
+          <table>
+              <thead>
+                <tr>
+                    <th>Az</th><th>Név</th><th>Nem</th><th>Szül. dátum</th><th>Nemzet</th>
+                </tr>
+              </thead>
+              <tbody>
+                { data.map((d, idx) => (
+                    <tr key={idx}>
+                        <td>{d.az}</td><td>{d.nev}</td><td>{d.nem}</td><td>{d.szuldat}</td><td>{d.nemzet}</td>
+                    </tr>
+                )) }
+              </tbody>
+          </table>
+      </div>
   )
 }
 
